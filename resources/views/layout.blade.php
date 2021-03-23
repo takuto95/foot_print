1af1c9db6bb598c680d50117e4d09c05748e7258
takuto <kaza.solution95@gmail.com>
2021-03-23 20:34:37 +0900

    CSS反映３

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link  href="{{ asset('css/style.min.css') }}" rel="stylesheet">
 </head>
 
 <body>
